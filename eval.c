42bd5536d48b0a612f9378fe104a30175d038db3
Adam Carruthers <adam.carruthers@mail.mcgill.ca>
2013-12-11 18:59:16 -0500

    updated evaluation to ensure variables don't get created outside of assignments

--- eval.c
+++ eval.c
@@ -1,6 +1,7 @@
 #include "eval.h"
 #include "stmt.h"
 #include "terp.h"
+#include "khash.h"
 
 #define NIL nil()
 
@@ -24,56 +25,63 @@ Element *evaluate(ParseNode *stmt, State *state) {
   case sASSIGN:
     // TODO: undeclared variables should not be added to state
     // TODO: set in stone types, or no? (Default: no)
-    // evaluate var (to add it to state)
+    // add var to state if it doesn't exist already
     if (!exists(state->h, stmt->children[0]->name)) {
       // set variable type to expression's value type
       stmt->children[0]->vType = stmt->children[1]->vType;
-      evaluate(stmt->children[0], state);
-    }
+      k = kh_put(32, state->h, stmt->children[0]->name, &ret);
+			// no need to evaluate variable, its value is being destroyed
+    } else {
+			// variable already exists in hashmap
+	    k = kh_get(32, state->h, stmt->children[0]->name);
+		}
 
-    k = kh_get(32, state->h, stmt->children[0]->name);
     kh_val(state->h, k) = evaluate(stmt->children[1], state);
 
     return NIL;
   case sIF:
     // evaluate branch iff cond = true
-    if (evaluate(stmt->children[0], state)) {
+		returnValue = evaluate(stmt->children[0], state);
+
+		if (returnValue->type == tNIL)
+			return NIL;
+
+    if (returnValue->value.boolean) {
       return evaluate(stmt->children[1], state);
     } else {
-      // change return type to tNIL
+			// there's no else branch of the statement, so its value becomes nil
       return NIL;
     }
   case sIFELSE:
-    // typecheck branches
-    if (stmt->children[1]->vType != stmt->children[2]->vType) {
-      error("Branch types don't match.");
-      return NIL;
-    }
-
     // evaluate b_true if cond = true else evaluate b_false
-    if (evaluate(stmt->children[0], state))
+		returnValue = evaluate(stmt->children[0], state);
+
+		if (returnValue->type == tNIL)
+			return NIL;
+
+    if (returnValue->value.boolean)
       return evaluate(stmt->children[1], state);
     else
       return evaluate(stmt->children[2], state);
   case sBOOL:
-    returnValue = (Element *)malloc(sizeof(Element));
-    returnValue->type = tBOOL;
-
-    // if no children, then it's true/false, no evaluation
+		// if no children, then it's true/false, no evaluation
     if (stmt->children == NULL) {
-      returnValue->value.boolean = stmt->value.boolean;
+  	  returnValue = (Element *)malloc(sizeof(Element));
+	    returnValue->type = tBOOL;
+		  returnValue->value.boolean = stmt->value.boolean;
       return returnValue;
     }
 
     // evaluate and store 0/1 in stmt->value
-    // make sure both sides resolve into an integer (should always as ints are initialized to 0)
     left = evaluate(stmt->children[0], state);
     right = evaluate(stmt->children[1], state);
 
-    if ((left->type != tINT && left->type != tREAL)
-        || (right->type != tINT && right->type != tREAL))
-      return NIL;
+		// TODO: this will need to be changed for reference types
+		if (left->type == tNIL || right->type == tNIL)
+			return NIL;
 
+    returnValue = (Element *)malloc(sizeof(Element));
+    returnValue->type = tBOOL;
     // TODO: handle real numbers
     switch(stmt->op.boolop) {
     case bLESSTHAN:
@@ -98,16 +106,10 @@ Element *evaluate(ParseNode *stmt, State *state) {
     returnValue->value = stmt->value;
     return returnValue;
   case sVAR:
-    // make sure variable is in state, if it isn't add it
+    // make sure variable is in state, if it isn't that's a bit of a problem
     if (!exists(state->h, stmt->name)) {
-      k = kh_put(32, state->h, stmt->name, &ret);
-      // the stmt->value must be the current true value of the variable
-      // (can't have been changed, hasn't been added to state yet)
-      returnValue = (Element *)malloc(sizeof(Element));
-      returnValue->type = stmt->vType;
-      returnValue->value = stmt->value;
-      kh_val(state->h, k) = returnValue;
-      return returnValue;
+      error("Variable doesn't exist");
+      return NIL;
     } else {
       // stmt->value might not be correct, obtain value from state
       k = kh_get(32, state->h, stmt->name);
